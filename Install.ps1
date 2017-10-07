7798a111c7bf94a986c36a8045914ec1505b692d
madd15 <joshdeakin@adam.com.au>
2017-10-07 13:15:27 +1030

    Changing Download File again

--- Install.ps1
+++ Install.ps1
@@ -48,40 +48,18 @@ function Expand-ZIPFile($file, $destination) {
 function Is64Bit {  
 	[IntPtr]::Size -eq 8  
 }
-function downloadFile($url, $targetFile) {
-    "Downloading $url"
-    $uri = New-Object "System.Uri" "$url"
-    $request = [System.Net.HttpWebRequest]::Create($uri)
-    $request.set_Timeout(15000) #15 second timeout
-    $response = $request.GetResponse()
-    $totalLength = [System.Math]::Floor($response.get_ContentLength()/1024)
-    $responseStream = $response.GetResponseStream()
-    $targetStream = New-Object -TypeName System.IO.FileStream -ArgumentList $targetFile, Create
-    $buffer = new-object byte[] 10KB
-    $count = $responseStream.Read($buffer,0,$buffer.length)
-    $downloadedBytes = $count
-    while ($count -gt 0)
-    {
-        [System.Console]::CursorLeft = 0
-        [System.Console]::Write("Downloaded {0}K of {1}K", [System.Math]::Floor($downloadedBytes/1024), $totalLength)
-        $targetStream.Write($buffer, 0, $count)
-        $count = $responseStream.Read($buffer,0,$buffer.length)
-        $downloadedBytes = $downloadedBytes + $count
-    }
-    "`nFinished Download"
-    $targetStream.Flush()
-    $targetStream.Close()
-    $targetStream.Dispose()
-    $responseStream.Dispose()
-} 
 
 #####################################
 Clear-Host
 title
 
 ## Download Web Platform Installer ##
-downloadFile($WPI, "wpi.msi")
-msiexec.exe /i wpi.msi /passive
+Write-Output "Downloading $WPI"
+$start_time = Get-Date
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($WPI, "$PSScriptRoot\wpi.msi")
+msiexec.exe /i '$PSScriptRoot\wpi.msi' /passive
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
 try {
     [reflection.assembly]::LoadWithPartialName("Microsoft.Web.PlatformInstaller") | Out-Null
